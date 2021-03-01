a663bfa9daaabc9ecd56ab41378ca27d6b024ac0
Jacob Krueger <jmk4u14@gmail.com>
2021-03-01 17:58:46 -0500

    Delete portfolio directory

--- portfolio/src/components/Home/index.js
+++
@@ -1,9 +0,0 @@
-import video from "../../assets/new-home-video.mp4"
-
-export default function Video() {
-    return (
-        <div class="videoFile">
-            <video playsInline autoPlay muted src={video} type="video/mp4"></video>
-        </div>
-    );
-}
